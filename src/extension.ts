350d0d5e210b00c54ec3b1e4bd8e44567f9831c1
czastack <2464851375@qq.com>
2021-03-03 00:11:39 +0800

    feat: add extension.nameStyleBig

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,30 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.nameStyle', function () {
-		// Get the active text editor
-		let editor = vscode.window.activeTextEditor;
+    context.subscriptions.push(vscode.commands.registerCommand('extension.nameStyle', nameStyle));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.nameStyleBig', nameStyle.bind(null, true)));
+}
 
-		if (editor) {
-			let document = editor.document;
-			let selection = editor.selection;
 
-			// Get the word within the selection
-			let word = document.getText(selection);
-			let ishump = word.match(/[A-Z]/);
-			
-			if (ishump) {
-				word = word.replace(/(?<=[a-z])([A-Z])/g, m => '_' + m.toLowerCase()).toLowerCase();
-			} else {
-				word = word.replace(/(?:_|^)(\w)/g, m => m.substr(-1).toUpperCase());
-			}
-			editor.edit(editBuilder => {
-				editBuilder.replace(selection, word);
-			});
-		}
-	});
+function nameStyle(big: Boolean) {
+    // Get the active text editor
+    const editor = vscode.window.activeTextEditor;
 
-	context.subscriptions.push(disposable);
+    if (editor) {
+        const document = editor.document;
+        const selection = editor.selection;
+
+        // Get the word within the selection
+        let word = document.getText(selection);
+        const isCamel = word.match(/[A-Z]/);
+        
+        if (isCamel) {
+            word = word.replace(/(?<=[a-z])([A-Z])/g, (m: string) => '_' + m.toLowerCase()).toLowerCase();
+        } else {
+            word = word.replace(big ? /(?:_|^)(\w)/g : /(?:_)(\w)/g, (m: string) => m.substr(-1).toUpperCase());
+        }
+        editor.edit(editBuilder => {
+            editBuilder.replace(selection, word);
+        });
+    }
 }
